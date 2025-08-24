3c6329d07e1655537dcafb0672bc1eced6b081a7
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-08-24 09:59:50 +0530

    feat: add page-header component and integrate into notifications view

--- public/js/components/core.js
+++ public/js/components/core.js
@@ -5,6 +5,7 @@ import "./feature-card.js";
 import "./activity-card.js";
 import "./activity-item.js";
 import "./status-item.js";
+import "./page-header.js";
 
 // Example: future components
 // import './ui-button.js';
