88f140c9af7c4e37229aae08ed1419a60e3725f6
hasna-2003 <hasnamass2003@gmail.com>
2025-09-06 22:24:31 +0530

    change the content inside notification bar
    change the content inside notification bar

--- src/Core/Navigation/NavigationConfig.php
+++ src/Core/Navigation/NavigationConfig.php
@@ -54,7 +54,7 @@ class NavigationConfig
                 ['title' => 'Daily Tasks', 'url' => '/collector/tasks', 'icon' => 'truck', 'description' => 'Manage pickups'],
                /* ['title' => 'Earnings', 'url' => '/collector/earnings', 'icon' => 'dollar-sign', 'description' => 'Track earnings'],*/
                 ['title' => 'Reports', 'url' => '/collector/analytics', 'icon' => 'chart-bar', 'description' => 'Performance reports'],
-                ['title' => 'Notification', 'url' => '/collector/notification', 'icon' => 'bell', 'description' => 'See notifications'],
+                ['title' => 'Notifications', 'url' => '/collector/notification', 'icon' => 'bell', 'description' => 'See notifications'],
                 ['title' => 'Profile', 'url' => '/collector/profile', 'icon' => 'user', 'description' => 'Your profile settings'],
             ],
             'company' => [
