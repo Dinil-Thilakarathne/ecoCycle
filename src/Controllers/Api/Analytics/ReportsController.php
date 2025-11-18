21ecf9465c2b444d187f126e9134ae54a76fc7b9
hasna-2003 <hasnamass2003@gmail.com>
2025-11-18 14:48:09 +0530

    created 2 controllrs

---
+++ src/Controllers/Api/Analytics/ReportsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ReportsController extends Controller
+{
+    public function wasteCollection()
+    {
+        return response()->json([
+            'total_collections' => 450,
+            'pending_collections' => 20,
+            'completed_collections' => 430
+        ]);
+    }
+
+    public function bidding()
+    {
+        return response()->json([
+            'total_bids' => 98,
+            'successful_bids' => 65,
+            'failed_bids' => 33
+        ]);
+    }
+
+    public function revenue()
+    {
+        return response()->json([
+            'monthly_revenue' => 78000,
+            'yearly_revenue' => 850000
+        ]);
+    }
+
+    public function export(Request $request)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Export generated'
+        ]);
+    }
+}
