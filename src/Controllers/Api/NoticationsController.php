f3ca26d8a6aa815c132c38678e930a3e672de984
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-11-22 23:08:09 +0530

    feat: Add API endpoint implementation guide and Notifications API controller.

---
+++ src/Controllers/Api/NoticationsController.php
@@ -0,0 +1,18 @@
+<?php
+namespace Controllers\Api;
+
+use Controllers\BaseController;
+use Core\Http\Request;
+use Core\Http\Response;
+
+class NoticationsController extends BaseController
+{
+    private CompanyModel $model;
+
+    public function __construct()
+    {
+
+
+    }
+
+}
